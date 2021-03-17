d1ec32ae4aa6b35afaf18d118fb53b1080613160
angelmrg <angel.ramos.gonzalez.al@iespoligonosur.org>
2021-03-17 14:30:04 +0100

    modales

--- src/main/java/gui/viewsandcontrollers/modal/ModalController.java
+++ src/main/java/gui/viewsandcontrollers/modal/ModalController.java
@@ -1,5 +1,32 @@
 package gui.viewsandcontrollers.modal;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class ModalController {
 
+    @FXML
+    private Label label;
+
+    public void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+    	
+    	Stage ventana = new Stage();
+    	Parent root = FMXLLoader.load(getClass().getResource(".../modal/Modal.fmxl"));
+    	Scene scene = new Scene(root);
+    	Label label = (Label)root.getChildrenUnmodifiable().get(0);
+    	label.setText(textfield.getText());
+    	ventana.setScene(scene);
+    	ventana.show();
+    }
 }
