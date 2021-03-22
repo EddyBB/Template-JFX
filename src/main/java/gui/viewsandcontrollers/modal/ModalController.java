226fbbb467392f019e860b067accfc4fbd0a9029
EddyBB <eddy.gonzalez.chavez.al@iespoligonosur.org>
2021-03-22 12:56:17 +0100

    fmxl

--- src/main/java/gui/viewsandcontrollers/modal/ModalController.java
+++ src/main/java/gui/viewsandcontrollers/modal/ModalController.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 public class ModalController {
 
